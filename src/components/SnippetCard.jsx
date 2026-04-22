5fc47520207c2bf25e0484b08a2224d476d48473
Mohammedsami001 <mohdsamisanadi@gmail.com>
2026-04-23 01:35:50 +0530

    Update syntax highlighter for OS practicals (C/C++)

--- src/components/SnippetCard.jsx
+++ src/components/SnippetCard.jsx
@@ -24,12 +24,13 @@ const SnippetCard = ({ title, code, icon }) => {
         .replace(/>/g, "&gt;");
 
       let styledLine = escapedLine
-        // Highlight keywords (Java & SQL)
-        .replace(/\b(public|private|protected|class|static|void|int|String|return|new|import|package|extends|implements|CREATE|TABLE|PRIMARY|KEY|VARCHAR|INT|DATE|TIME|INSERT|INTO|VALUES|SELECT|FROM|WHERE|COUNT|AVG|SUM|MIN|MAX|AS|GROUP|ORDER|BY|DESC|ASC|HAVING|JOIN|INNER|LEFT|RIGHT|FULL|OUTER|ON|UNION|INTERSECT|MINUS|VIEW|TRIGGER|AFTER|BEFORE|BEGIN|END|EXISTS|NOT|GRANT|REVOKE|PROCEDURE|IN|OUT|DATABASE|USE|DROP|ALTER|ADD|CONSTRAINT|FOREIGN|REFERENCES|CHECK|UNIQUE|DEFAULT|INDEX|VIEW|PROCEDURE|FUNCTION|TRIGGER)\b/gi, '<span class="keyword">$1</span>')
-        // Highlight Class Names (Starts with Uppercase, not a keyword)
-        .replace(/\b(?!(?:public|private|protected|class|static|void|int|String|return|new|import|package|extends|implements|CREATE|TABLE|PRIMARY|KEY|VARCHAR|INT|DATE|TIME|INSERT|INTO|VALUES|SELECT|FROM|WHERE|COUNT|AVG|SUM|MIN|MAX|AS|GROUP|ORDER|BY|DESC|ASC|HAVING|JOIN|INNER|LEFT|RIGHT|FULL|OUTER|ON|UNION|INTERSECT|MINUS|VIEW|TRIGGER|AFTER|BEFORE|BEGIN|END|EXISTS|NOT|GRANT|REVOKE|PROCEDURE|IN|OUT|DATABASE|USE|DROP|ALTER|ADD|CONSTRAINT|FOREIGN|REFERENCES|CHECK|UNIQUE|DEFAULT|INDEX|VIEW|PROCEDURE|FUNCTION|TRIGGER)\b)([A-Z][a-zA-Z0-9]+)\b/g, '<span class="class-name">$1</span>')
+        // Highlight keywords (Java, SQL, & C)
+        .replace(/\b(public|private|protected|class|static|void|int|String|return|new|import|package|extends|implements|CREATE|TABLE|PRIMARY|KEY|VARCHAR|INT|DATE|TIME|INSERT|INTO|VALUES|SELECT|FROM|WHERE|COUNT|AVG|SUM|MIN|MAX|AS|GROUP|ORDER|BY|DESC|ASC|HAVING|JOIN|INNER|LEFT|RIGHT|FULL|OUTER|ON|UNION|INTERSECT|MINUS|EXCEPT|VIEW|TRIGGER|AFTER|BEFORE|BEGIN|END|EXISTS|NOT|GRANT|REVOKE|PROCEDURE|IN|OUT|DATABASE|USE|DROP|ALTER|ADD|CONSTRAINT|FOREIGN|REFERENCES|CHECK|UNIQUE|DEFAULT|INDEX|VIEW|PROCEDURE|FUNCTION|TRIGGER|printf|main|char|#include|#define|pid_t|fork|open|write|read|close|lseek|SEEK_SET|O_CREAT|O_RDWR)\b/gi, '<span class="keyword">$1</span>')
+        // Highlight Class Names / Data Types (Starts with Uppercase or specific types)
+        .replace(/\b(?!(?:public|private|protected|class|static|void|int|String|return|new|import|package|extends|implements|CREATE|TABLE|PRIMARY|KEY|VARCHAR|INT|DATE|TIME|INSERT|INTO|VALUES|SELECT|FROM|WHERE|COUNT|AVG|SUM|MIN|MAX|AS|GROUP|ORDER|BY|DESC|ASC|HAVING|JOIN|INNER|LEFT|RIGHT|FULL|OUTER|ON|UNION|INTERSECT|MINUS|EXCEPT|VIEW|TRIGGER|AFTER|BEFORE|BEGIN|END|EXISTS|NOT|GRANT|REVOKE|PROCEDURE|IN|OUT|DATABASE|USE|DROP|ALTER|ADD|CONSTRAINT|FOREIGN|REFERENCES|CHECK|UNIQUE|DEFAULT|INDEX|VIEW|PROCEDURE|FUNCTION|TRIGGER|printf|main|char|#include|#define|pid_t|fork|open|write|read|close|lseek|SEEK_SET|O_CREAT|O_RDWR)\b)([A-Z][a-zA-Z0-9]+)\b/g, '<span class="class-name">$1</span>')
         .replace(/"([^"]*)"/g, '<span class="string">"$1"</span>')
-        .replace(/\/\/.*$/g, '<span class="comment">$&</span>');
+        .replace(/\/\/.*$/g, '<span class="comment">$&</span>')
+        .replace(/\/\*[\s\S]*?\*\//g, '<span class="comment">$&</span>');
 
       return (
         <div key={i} dangerouslySetInnerHTML={{ __html: styledLine || '&nbsp;' }} />
